837f45308c52fb348b1fcc595c0e931ff38b64ef
Daryll Wong <daryll_wong@hotmail.com>
2020-11-20 00:27:00 +0800

    basic template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-rest-mysql-basic-template
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clojure_rest_mysql_basic_template/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-rest-mysql-basic-template.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
